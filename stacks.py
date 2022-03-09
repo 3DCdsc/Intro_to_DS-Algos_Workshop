7905850c2727874ff90b286fdb44c6b7423074a0
Beckham <72685677+bckhm@users.noreply.github.com>
2022-03-09 19:13:27 +0800

    Add code for students
    Linked lists, Stacks and Queues file for a code along session

---
+++ stacks.py
@@ -0,0 +1,31 @@
+# This is a stack class
+class Stack:
+    """Stacks store items in a list"""
+    def __init__(self):
+        # Write code here
+
+    """Pop removes last item of the stack"""
+    def pop(self):
+        # Write code here
+
+
+    """Push adds an item to the back of the stack"""
+    def push(self, item):
+        # Write code here
+
+    """Peek returns the last item of the stack WITHOUT removing it"""
+    def peek(self):
+        # Write code here
+
+    """isEmpty checks if the stack is Empty"""
+    def isEmpty(self):
+        return self.items == []
+
+    """Size returns the number of items in the stack"""
+    def size(self):
+        return len(self.items)
+
+
+# Test Code
+
+
